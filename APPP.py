541b9ecaf01904205e74adab8a9e2c55346039de
Ola Maki <olamaki483@gmail.com>
2025-08-02 00:08:02 +0300

    Added final backend and security configuration

--- APPP.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    'host': 'dpg-d24fupggjchc73a9urr0-a.frankfurt-postgres.render.com',
-    'port': '5432',
-    'user': 'madad_user',
-    'password': 'iNTWzvPNACpkDLLPzorNTGhl7hXFljdY',
-    'database': 'madad_services_db'
-}
